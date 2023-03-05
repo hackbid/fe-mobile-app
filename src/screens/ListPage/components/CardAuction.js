cee75f7bd533f0b7b31c113a3496f0644dd60693
dwikiop <dwikiokvianp@gmail.com>
2023-03-05 22:04:04 +0700

    feat - add auction list to card auction and navigation to it card

---
+++ src/screens/ListPage/components/CardAuction.js
@@ -0,0 +1,71 @@
+import {
+  StyleSheet,
+  View,
+  Text,
+  useWindowDimensions,
+  TouchableNativeFeedback,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function CardAuction({ item }) {
+  const navigation = useNavigation();
+  const { height, width } = useWindowDimensions();
+  return (
+    <View
+      className="m-2 rounded-md"
+      style={{
+        height: height / 4,
+        width: width / 2.3,
+        backgroundColor: "#E5E5E5",
+      }}
+    >
+      <TouchableNativeFeedback
+        onPress={() => {
+          navigation.navigate("DetailItem", { item });
+        }}
+      >
+        <View style={styles.container}>
+          <View style={styles.headerText}>
+            <Text style={styles.textTitle}>{item.title}</Text>
+            <Text style={styles.textComingSoon}>COMING SOON</Text>
+          </View>
+          <View style={styles.bottomText}>
+            <Text style={styles.textDescription}>
+              {item.description.substring(0, 50)}...
+            </Text>
+            <Text style={styles.textDate} className="text-right">
+              {item.date}
+            </Text>
+          </View>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: "10%",
+    flex: 1,
+  },
+  headerText: {
+    flex: 1,
+  },
+  textTitle: {
+    fontWeight: "700",
+  },
+  textComingSoon: {
+    fontWeight: "700",
+    color: "#0F721F",
+  },
+  bottomText: {
+    flex: 2,
+  },
+  textDescription: {
+    flex: 4,
+  },
+  textDate: {
+    flex: 1,
+    fontWeight: "400",
+  },
+});
--- src/store/actions/actionCreator.js
+++ src/store/actions/actionCreator.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { FETCH_CATEGORIES, LOGIN_SUCCESS, LOGOUT_SUCCESS } from './ActionType';
-const SERVER_URL = 'http://192.168.1.5:4000';
+const SERVER_URL = 'http:192.168.1.10:4000';
 
 //FOR STATE
 export const changeLogin = (data) => {
