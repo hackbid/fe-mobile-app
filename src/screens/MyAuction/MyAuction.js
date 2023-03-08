845b447d13c683712bf172e311dc757e191ea360
dwikiop <dwikiokvianp@gmail.com>
2023-03-08 14:04:52 +0700

    feat - add my auction list, and helper capitalize to uppercase the status and add my Auction card

--- src/screens/MyAuction/MyAuction.js
+++ src/screens/MyAuction/MyAuction.js
@@ -1,39 +1,48 @@
-import { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { getMyAuction } from '../../store/actions/actionCreator';
+import { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { getMyAuction } from "../../store/actions/actionCreator";
+import MyAuctionCard from "./components/MyAuctionCard";
 
 export default function MyAuction({ navigation, route }) {
-    const { user } = useSelector((state) => state.user);
-    const [auctionList, setAuctionList] = useState([]);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getMyAuction(user.id)).then((res) => {
-            setAuctionList(res);
-        });
-    }, []);
+  const { user } = useSelector((state) => state.user);
+  const [auctionList, setAuctionList] = useState([]);
+  const dispatch = useDispatch();
 
-    return (
-        <View>
-            <TouchableOpacity
-                className='bg-blue-800 mx-1 p-3 px-4 rounded-lg my-2 '
-                onPress={() => {
-                    navigation.navigate('PostItem');
-                }}>
-                <Text className='text-center text-white'>Add new Item</Text>
-            </TouchableOpacity>
-            <Text>MyAuction</Text>
-            <View className='w-[80%] mx-auto'>
-                {auctionList.map((item) => {
-                    return (
-                        <View key={item.id} className='p-3 bg-white my-3'>
-                            <Text>{item.name}</Text>
-                            <Text>{item.description}</Text>
-                            <Text>{item.startPrice}</Text>
-                        </View>
-                    );
-                })}
-            </View>
-        </View>
-    );
+  const { height, width } = useWindowDimensions();
+
+  useEffect(() => {
+    dispatch(getMyAuction(user.id)).then((res) => {
+      console.log(res);
+      setAuctionList(res);
+    });
+  }, []);
+
+  return (
+    <View>
+      <View className="w-[90%] mx-auto mt-4">
+        <Text className="text-2xl font-extrabold text-gray-700">
+          My Auction
+        </Text>
+      </View>
+      <View className="w-[90%] mx-auto mt-4">
+        {auctionList.map((item) => {
+          return <MyAuctionCard key={item.id} item={item} />;
+        })}
+      </View>
+      <TouchableOpacity
+        className="bg-blue-800 mx-1 p-3 px-4 rounded-lg my-2 w-[90%] mx-auto mt-4"
+        onPress={() => {
+          navigation.navigate("PostItem");
+        }}
+      >
+        <Text className="text-center text-white">Add new Item</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
